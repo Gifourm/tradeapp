68cdf22c13c92c13a7e4548757a91fcb9c15dc57
Gifourm <g4rm@mail.ru>
2023-09-19 20:47:13 +0300

    Rm tk

--- notification.py
+++ notification.py
@@ -2,7 +2,7 @@ from requests import post
 
 
 def send_telegram(chat_id: int, message: str) -> None:
-    token = "5594498832:AAEYuMa3yAhm8OrEg8SM5zU9KgSvjzbsCjo"
+    token = "my_token"
     telegram_url = f"https://api.telegram.org/bot{token}/sendMessage"
     request = post(telegram_url, data={
         "chat_id": chat_id,
